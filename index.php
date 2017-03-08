a3025edbce62fafad6b15d9ee0eb15014cde64f2
SirGeerald <1510605@rgu.ac.uk>
2017-03-08 02:25:28 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+
+<!-- HEADER START -->
+<header>
+
+<h1> Marvel Movies</h1>
+</header>
+<!-- HEADER END -->
+
+
+<!-- MAIN START -->
+<main>
+    <a href="displayall.php"> <button type="button">Display All Movies</button> </a>
+
+    <a href="displaymarvel.php"> <button type="button">Display All Marvel Movies </button> </a>
+    <a href="displayafter2010.php"> <button type="button">Display Created after 2010</button> </a>
+    <a href="displayxmen.php"> <button type="button">Display All X-Men Films</button> </a>
+
+</main>
+<!-- MAIN END -->
+
+
+<!-- FOOTER START -->
+<footer>
+
+
+</footer>
+<!-- FOOTER END -->
+
+
+</body>
+</html>
